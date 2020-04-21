0e92568201d75200addf975c4350802ed34962ba
朱满要 <1140938685@qq.com>
2020-04-21 22:06:28 +0800

    修改

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,13 +1,13 @@
 /***
- * @FileName:  首页index的 js
- * @Author: manyao.zhu
- * @Date: 2020-01-02 17:19:30
- */
+ * @FileName:  首页index的 js
+ * @Author: manyao.zhu
+ * @Date: 2020-01-02 17:19:30
+ */
 
 function login() {
-  $.post('/auth/login', $("#login").serializeArray(), function(res){
-    console.log('index', res)
-    if(res.status) {
+  $.post('/auth/login', $('#login').serializeArray(), function (res) {
+    console.log('index', res);
+    if (res.status) {
       window.location.href = '/t1';
     }
   });
