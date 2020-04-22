37385080800a342d4f5ef15349793a62202ee06c
zhumanyao <zhumanyao@dev.ithinkdt.com>
2020-04-22 18:06:51 +0800

    性怎

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -4,11 +4,59 @@
  * @Date: 2020-01-02 17:19:30
  */
 
-function login() {
-  $.post('/auth/login', $('#login').serializeArray(), function (res) {
-    console.log('index', res);
-    if (res.status) {
-      window.location.href = '/t1';
-    }
-  });
+// function login() {
+//   $.post('/auth/login', $('#login').serializeArray(), function (res) {
+//     console.log('index', res);
+//     if (res.status) {
+//       window.location.href = '/t1';
+//     }
+//   });
+// }
+
+var index = 0;
+
+// 宽度
+const domWidth = $('.dt-case-content').width();
+
+// 复制第一个并天叫道盒子里面
+const listClone = $('.dt-case-list')
+  .first()
+  .clone();
+$('.dt-case--list-content').append(listClone);
+
+// 数量
+const listLen = $('.dt-case-list').length;
+
+// 点击向左按钮
+$('.dt-case-left').click(function() {
+  toLeft('dt-case--list-content', listLen);
+});
+
+// 点击向右按钮
+$('.dt-case-right').click(function() {
+  toRight('dt-case--list-content', listLen);
+});
+
+// 向左移动
+function toLeft(type, size) {
+  index++;
+  if (index === size) {
+    $(`.${type}`).css({ left: 0 });
+    index = 1;
+  }
+  $(`.${type}`)
+    .stop()
+    .animate({ left: -index * domWidth }, 1000);
+}
+
+// 向右运动
+function toRight(type, size) {
+  index--;
+  if (index === -1) {
+    $(`.${type}`).css({ left: -(size - 1) * domWidth });
+    index = size - 2;
+  }
+  $(`.${type}`)
+    .stop()
+    .animate({ left: -index * domWidth }, 1000);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,48 @@
 /***
- * @FileName: 首页路由
- * @Author: manyao.zhu
- * @Date: 2019-12-17 11:45:19
- */
+ * @FileName: 首页路由
+ * @Author: manyao.zhu
+ * @Date: 2019-12-17 11:45:19
+ */
 var express = require('express');
 var router = express.Router();
 
-const indexService = require('../fetch/service/index.service')
-const resultBean = require('../utils/result-bean')
+const indexService = require('../fetch/service/index.service');
+const resultBean = require('../utils/result-bean');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/auth/login', (req, res, next) => {
-  indexService.login({username:req.body.username, password:req.body.password}).then(result=> {
-    if (result.status) {
-      req.session.access_token = result.data;
-      res.send(resultBean.success('登录成功'))
-    }
-  })
-})
+router.get('/product', function(req, res, next) {
+  res.render('product');
+});
 
+router.get('/service', function(req, res, next) {
+  res.render('service');
+});
+
+router.get('/us', function(req, res, next) {
+  res.render('us');
+});
+
+router.get('/case_xbom', function(req, res, next) {
+  res.render('caseone');
+});
+
+router.get('/case_alm', function(req, res, next) {
+  res.render('casetwo');
+});
+
+router.post('/auth/login', (req, res, next) => {
+  indexService
+    .login({ username: req.body.username, password: req.body.password })
+    .then(result => {
+      if (result.status) {
+        req.session.access_token = result.data;
+        res.send(resultBean.success('登录成功'));
+      }
+    });
+});
 
 module.exports = router;
